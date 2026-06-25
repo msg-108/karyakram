d74b79762473b702e8bca01ccf6afedcfb1c4a1e
Madhusudhan Gharti <246828956+msg-108@users.noreply.github.com>
2026-06-25 09:27:20 +0000

    feat(user): setup initial model for users

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,9 +1,40 @@
-from django.contrib.auth.models import AbstractUser
+﻿from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('user', 'User'),
+        ('organizer', 'Organizer'),
+    )
+
+    CITIZENSHIP_CHOICES = (
+        ('NP', 'Nepali'),
+    )
+
+    # Basic Fields
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
     email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=14, blank=True, unique=True, null=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    is_email_verified = models.BooleanField(default=False)
+    is_phone_verified = models.BooleanField(default=False)
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Organizer Specific Fields
+    organization_name = models.CharField(max_length=255, blank=True, null=True)
+    citizenship = models.CharField(max_length=2, choices=CITIZENSHIP_CHOICES, blank=True, null=True)
+    citizenship_number = models.CharField(max_length=14, blank=True, null=True, unique=True)
+    pan_number = models.CharField(max_length=9, blank=True, null=True)
+    bank_name = models.CharField(max_length=255, blank=True, null=True)
+    bank_account_number = models.CharField(max_length=20, blank=True, null=True)
+    is_organizer_approved = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
