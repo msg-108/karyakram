606f2e7747c0e70e2d1fe36fd4a1411206c5fffa
Madhusudhan Gharti <246828956+msg-108@users.noreply.github.com>
2026-06-25 16:09:23 +0000

    feat(user): add urls for login, refresh and logout

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import RegisterView, ProfileView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+
+)
+from .views import RegisterView, ProfileView, LogoutView
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='login'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('auth/logout/', LogoutView.as_view(), name='logout'),
 ]
