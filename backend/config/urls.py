1ca24203a31d7caa71236723a53114b2acac22d7
Madhusudhan Gharti <246828956+msg-108@users.noreply.github.com>
2026-06-26 03:42:18 +0000

    feat(dashboard): setup initial urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.users.urls')),
+    path('api/dashboard/', include('apps.dashboard.urls')),
 ]
